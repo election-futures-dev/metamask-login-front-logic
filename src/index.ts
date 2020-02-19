01868c46cd30bf125e2b2a71a9c2e85639027a4b
zachary <syyun@snu.ac.kr>
2020-02-19 18:18:21 +0900

    Add GetSignature

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,62 @@
-// import Web3 from 'web3';
+import Web3 from 'web3';
 
-declare const window: any;
-// declare const web3: Web3;
+declare let window: any;
+declare let web3: Web3;
 
 export const Greeter = (name: string) => `Hello ${name}`;
 
-export async function CheckMetamaskInstalled() {
-  if (!(window as any).ethereum) {
+function createNonce() {
+
+  var dict = "abcdefghijklmnopqrstuvwxyz0123456789".split("");
+  var result = "";
+
+  for (var i = 0; i < 10; i++) {
+    var rand = Math.floor(Math.random() * 36);
+    result += dict[rand];
+  }
+
+  return result;
+
+}
+
+async function CheckMetamaskInstalled() {
+  if (!window.ethereum) {
     window.alert('Please install MetaMask first.');
+    return
   } else {
+    web3 = new Web3(window.ethereum);
     return true;
   }
 }
 
-export async function GetPublicAddress() {
+async function GetPublicAddress() {
   try {
-    const accounts = await window.ethereum.enable();
-    return accounts;
+    let publicAddressArray: string[] = await window.ethereum.enable();
+    let publicAddress = publicAddressArray[0]
+    return publicAddress
   } catch (error) {
     window.alert('You need to allow MetaMask.');
-    return false;
+    return
   }
+}
+
+async function GetSignature(publicAddress: string) {
+  var nonce = createNonce()
+  let signature = web3.eth.personal.sign(nonce, publicAddress, '')
+  return signature
 }
 
 export async function Authenticate() {
   CheckMetamaskInstalled().then(installed => {
     if (installed) {
-      GetPublicAddress().then(address => {
-        if (address) {
-          window.alert(address);
+      GetPublicAddress().then(publicAddress => {
+        if (publicAddress) {
+          // window.alert(publicAddress);
+          let signature = GetSignature(publicAddress)
+          window.alert(signature)
         }
       });
     }
   });
 }
+
