e16d17314a0b8b85aac12b1ce9d6dafe525ac582
zachary <syyun@snu.ac.kr>
2020-02-19 15:47:27 +0900

    Add fragmented web3 install checker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
+import Web3 from 'web3';
+
+let web3: Web3 | undefined = undefined;
+
 declare const window: any;
 
-export const Greeter = (name: string) => `Hello ${name}`;
-export const ButtonClicked = () => {
-  window.ethereum.enable();
+// export const Greeter = (name: string) => `Hello ${name}`;
+
+export function CheckMetamaskInstalled(): void {
+  if (!(window as any).ethereum) {
+    window.alert('Please install MetaMask first.');
+  }
 };
+
+export async function GetPublicAddress() {
+  if (!web3) {
+    try {
+      const accounts = await window.ethereum.enable()
+      web3 = new Web3(window.ethereum);
+    } catch (error) {
+      window.alert('You need to allow MetaMask.');
+      return;
+    }
+  }
+}
