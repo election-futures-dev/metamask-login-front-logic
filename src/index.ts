e53f9b9155f6f3173cf11a69600a77852e4552d3
zachary <syyun@snu.ac.kr>
2020-02-19 16:30:22 +0900

    Comment out web3 relevant

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import Web3 from 'web3';
 
-let web3: Web3;
-
+// declare const web3: Web3;
 declare const window: any;
 
 // export const Greeter = (name: string) => `Hello ${name}`;
@@ -12,14 +11,14 @@ export function CheckMetamaskInstalled(): void {
   }
 }
 
-export async function GetPublicAddress() {
-  if (!web3) {
-    try {
-      const accounts = await window.ethereum.enable();
-      web3 = new Web3(window.ethereum);
-    } catch (error) {
-      window.alert('You need to allow MetaMask.');
-      return;
-    }
-  }
-}
+// export async function GetPublicAddress() {
+//   if (!web3) {
+//     try {
+//       const accounts = await window.ethereum.enable();
+//       // web3 = new Web3(window.ethereum);
+//     } catch (error) {
+//       window.alert('You need to allow MetaMask.');
+//       return;
+//     }
+//   }
+// }
