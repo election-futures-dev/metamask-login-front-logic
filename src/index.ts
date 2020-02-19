e9cb6b3419dc792a1d5b3badb082b3efaa8f957b
zachary <syyun@snu.ac.kr>
2020-02-19 17:26:07 +0900

    Add Authenticate function as a entire process wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
-import Web3 from 'web3';
+// import Web3 from 'web3';
 
 declare const window: any;
-declare const web3: Web3;
+// declare const web3: Web3;
 
 export const Greeter = (name: string) => `Hello ${name}`;
 
-export function CheckMetamaskInstalled(): void {
+export async function CheckMetamaskInstalled() {
   if (!(window as any).ethereum) {
     window.alert('Please install MetaMask first.');
+  } else {
+    return true
   }
 }
 
 export async function GetPublicAddress() {
-  if (!web3) {
-    try {
-      const accounts = await window.ethereum.enable();
-      window.alert(`${JSON.stringify(accounts)}`);
-      // web3 = new Web3(window.ethereum);
-    } catch (error) {
-      window.alert('You need to allow MetaMask.');
-      return;
-    }
-  } else {
-    window.alert('!web == false');
+  try {
+    const accounts = await window.ethereum.enable();
+    return accounts
+  } catch (error) {
+    window.alert('You need to allow MetaMask.');
+    return false;
   }
+}
+
+export async function Authenticate() {
+  CheckMetamaskInstalled().then(value => {
+    if (value) {
+      GetPublicAddress().then(value => {
+        if (value) {
+          window.alert(value)
+        }
+      })
+    }
+  })
 }
